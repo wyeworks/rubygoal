bfaa53a7950cec03f5890931f5241fa637bfaef6
Jorge Bejar <jorge@wyeworks.com>
2015-06-07 18:19:39 -0300

    Add new player tests

---
+++ test/players/goalkeeper_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+module Rubygoal
+  class GaolKeeperPlayerTest < Minitest::Test
+    def setup
+      @game = Game.new
+      home_team = game.team_home
+
+      @player = home_team.players.values.first
+
+      initial_pos = Field.absolute_position(
+        Team.initial_player_positions.first,
+        :home
+      )
+      @player.position = initial_pos
+    end
+
+    def test_goalkeeper_is_already_covering_goal
+      goal_pos = Field.goal_position(:home)
+      game.ball.position = Position.new(300, goal_pos.y)
+
+      player.move_to_cover_goal(game.ball)
+      player.update(elapsed_time)
+
+      assert_equal Velocity.new(0, 0), player.velocity
+    end
+
+    private
+
+    attr_reader :game, :player
+
+    def elapsed_time
+      1 / 60.0
+    end
+  end
+end
