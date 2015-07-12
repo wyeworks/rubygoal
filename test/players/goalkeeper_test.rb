124c2c6aedafebf40770c92cf6a2267cfa857675
Jorge Bejar <jorge@wyeworks.com>
2015-07-12 17:32:39 -0300

    More tests for goalkeeper movement

--- test/players/goalkeeper_test.rb
+++ test/players/goalkeeper_test.rb
@@ -20,6 +20,24 @@ module Rubygoal
       assert_equal Velocity.new(0, 0), player.velocity
     end
 
+    def test_goalkeeper_moves_down_to_cover_goal
+      game.ball.position = Position.new(300, 300)
+
+      player.move_to_cover_goal(game.ball)
+      player.update(elapsed_time)
+
+      assert_in_delta Velocity.new(0, -3.5), player.velocity, 0.001
+    end
+
+    def test_goalkeeper_moves_up_to_cover_goal
+      game.ball.position = Position.new(300, 900)
+
+      player.move_to_cover_goal(game.ball)
+      player.update(elapsed_time)
+
+      assert_in_delta Velocity.new(0, 3.5), player.velocity, 0.001
+    end
+
     private
 
     attr_reader :game, :player
