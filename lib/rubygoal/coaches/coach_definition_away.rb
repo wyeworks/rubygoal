6b9748e661056d309f8b14409b39cccb2be00850
Jorge Bejar <jorge@wyeworks.com>
2015-09-02 00:13:48 -0300

    WIP temp changes in default coach files

--- lib/rubygoal/coaches/coach_definition_away.rb
+++ lib/rubygoal/coaches/coach_definition_away.rb
@@ -5,7 +5,7 @@ module Rubygoal
   class CoachDefinitionAway < CoachDefinition
 
     team do
-      name "Argentina"
+      name "Brasil"
     end
 
     def formation(match)
@@ -18,43 +18,43 @@ module Rubygoal
           att_midfielders :average3
           attackers :none, :none, :fast2, :average4, :none
         end
-      elsif match.me.draw?
-        formation.lineup do
-          lines do
-            defenders 13
-            midfielders 40
-            attackers 65
-          end
+      #elsif match.me.draw?
+        #formation.lineup do
+          #lines do
+            #defenders 13
+            #midfielders 40
+            #attackers 65
+          #end
 
-          defenders :average1, :fast1, :none, :average3, :average5
-          midfielders :average2, :none, :none, :none, :average6
-          attackers :none, :fast2, :none, :average4, :none
+          #defenders :average1, :fast1, :none, :average3, :average5
+          #midfielders :average2, :none, :none, :none, :average6
+          #attackers :none, :fast2, :none, :average4, :none
 
-          custom_position do
-            player :fast3
-            position 30, 10
-          end
-          custom_position do
-            player :captain
-            position match.ball.x, match.ball.y
-          end
-        end
-      elsif match.me.losing?
-        if match.time < 30
-          formation.lineup do
-            defenders :none, :average2, :average4, :average5, :none
-            midfielders :average1, :none, :none, :fast2, :average6
-            attackers :none, :average3, :none, :fast3, :none
+          #custom_position do
+            #player :fast3
+            #position 30, 10
+          #end
+          #custom_position do
+            #player :captain
+            #position match.ball.x, match.ball.y
+          #end
+        #end
+      #elsif match.me.losing?
+        #if match.time < 30
+          #formation.lineup do
+            #defenders :none, :average2, :average4, :average5, :none
+            #midfielders :average1, :none, :none, :fast2, :average6
+            #attackers :none, :average3, :none, :fast3, :none
 
-            custom_position do
-              player :fast1
-              position 33, 50
-            end
-            custom_position do
-              player :captain
-              position 67, 50
-            end
-          end
+            #custom_position do
+              #player :fast1
+              #position 33, 50
+            #end
+            #custom_position do
+              #player :captain
+              #position 67, 50
+            #end
+          #end
         else
           formation.lineup do
             defenders :none, :average2, :fast1, :average5, :none
@@ -64,7 +64,7 @@ module Rubygoal
             attackers :none, :average3, :none, :fast3, :none
           end
         end
-      end
+      #end
 
       formation
     end
--- lib/rubygoal/coaches/coach_definition_home.rb
+++ lib/rubygoal/coaches/coach_definition_home.rb
@@ -5,7 +5,7 @@ module Rubygoal
   class CoachDefinitionHome < CoachDefinition
 
     team do
-      name "Uruguay"
+      name "Colombia"
 
       players do
         captain :godin
@@ -26,49 +26,49 @@ module Rubygoal
     def formation(match)
       formation = Formation.new
 
-      if match.me.winning?
-        formation.defenders :pereira, :cacerez, :gimenez, :godin, :rodriguez
-        formation.midfielders :lodeiro, :none, :rolan, :none, :arevalo
-        formation.attackers :none, :cavani, :none, :suarez, :none
-      elsif match.time < 20
+      #if match.me.winning?
+        #formation.defenders :pereira, :cacerez, :gimenez, :godin, :rodriguez
+        #formation.midfielders :lodeiro, :none, :rolan, :none, :arevalo
+        #formation.attackers :none, :cavani, :none, :suarez, :none
+      #elsif match.time < 20
         formation.defenders :none, :rolan, :cacerez, :gimenez, :none
         formation.midfielders :arevalo, :lodeiro, :godin, :none, :pereira
         formation.attackers :suarez, :none, :none, :cavani, :rodriguez
-      elsif match.time < 20 && match.me.winning?
-        # Mirror opponent players
+      #elsif match.time < 20 && match.me.winning?
+        ## Mirror opponent players
 
-        opponent = match.other.positions
-        my_players = players
+        #opponent = match.other.positions
+        #my_players = players
 
-        opponent.each_with_index do |(opponent_name, opponent_pos), index|
-          formation.lineup do
-            custom_position do
-              player my_players[index].name
-              position opponent_pos.x, 100.0 - opponent_pos.y
-            end
-          end
-        end
-      else
-        formation.lineup do
-          if match.ball.x < 50
-            lines do
-              defenders 10
-              midfielders 30
-              attackers 50
-            end
-          else
-            lines do
-              defenders 30
-              midfielders 50
-              attackers 70
-            end
-          end
+        #opponent.each_with_index do |(opponent_name, opponent_pos), index|
+          #formation.lineup do
+            #custom_position do
+              #player my_players[index].name
+              #position opponent_pos.x, 100.0 - opponent_pos.y
+            #end
+          #end
+        #end
+      #else
+        #formation.lineup do
+          #if match.ball.x < 50
+            #lines do
+              #defenders 10
+              #midfielders 30
+              #attackers 60
+            #end
+          #else
+            #lines do
+              #defenders 30
+              #midfielders 55
+              #attackers 80
+            #end
+          #end
 
-          defenders :pereira, :cacerez, :gimenez, :godin, :rodriguez
-          midfielders :lodeiro, :none, :rolan, :none, :arevalo
-          attackers :none, :cavani, :none, :suarez, :none
-        end
-      end
+          #defenders :pereira, :cacerez, :gimenez, :godin, :rodriguez
+          #midfielders :lodeiro, :none, :rolan, :none, :arevalo
+          #attackers :none, :cavani, :none, :suarez, :none
+        #end
+      #end
 
       formation
     end
